0cf2f19c509971406b2b4643ed20a6c4180a6ee0
Luca Negrette <lucanegrette@hotmail.com>
2020-01-09 19:12:49 -0500

    MySQL1.0

---
+++ MySQL/Sql.py
@@ -0,0 +1,63 @@
+# apt install python3-dev default-libmysqlclient-dev
+# pip install mysqlclient
+
+import MySQLdb
+
+
+class Sql:
+
+    HOSTNAME = "127.0.0.1"
+    USERNAME = "luca13-cmyk"
+    PASSWORD = "toor"
+    DBNAME = "python"
+
+    def __init__(self):
+        try:
+            self.__conn = MySQLdb.connect(
+                db=Sql.DBNAME,
+                host=Sql.HOSTNAME,
+                user=Sql.USERNAME,
+                passwd=Sql.PASSWORD
+            )
+        except MySQLdb.Error as e:
+            print(f"Error na conexao ao MySQL Server: {e}")
+
+    @property
+    def conn(self):
+        return self.__conn
+
+    def disconnect(self):
+        if self.conn:
+            return self.conn.close()
+
+    def query(self, stmt):
+        cur = self.conn.cursor()
+        cur.execute(stmt)
+        self.commit()
+
+    def select(self, stmt):
+        cur = self.conn.cursor()
+        cur.execute(stmt)
+        self.commit()
+        return cur.fetchall()
+
+    def commit(self):
+        self.conn.commit()
+
+    @classmethod
+    def change_conn(cls, hostname, username, password, dbname):
+        cls.HOSTNAME = hostname
+        cls.USERNAME = username
+        cls.PASSWORD = password
+        cls.DBNAME = dbname
+
+
+sql = Sql()
+
+ret = sql.select("SELECT * FROM teste")
+print(ret)
+
+Sql.change_conn("127.0.0.1", "root", "toor", "python")
+sql2 = Sql()
+
+
